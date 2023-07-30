1c3bc19da3fca99d9b805c1374214c63eff85d72
Jared Cover <jhcover@gmail.com>
2023-07-30 14:21:28 -0400

    update twitter logo

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import kick from "./images/kick.png"
 import substack from "./images/substack.png"
 import twitch from "./images/twitch.png"
 import twitter from "./images/twitter.svg"
+import newTwitter from "./images/Featured Twitter New Logo.jpg"
 import youtube from "./images/youtube.svg"
 
 function App() {
@@ -11,14 +12,14 @@ function App() {
         <div style={{display: "flex", justifyContent: "flex-end", margin: 18}}>
             <div style={{display: "flex", flexDirection:"column", alignItems: "center",}}>
                 <div style={{display: "flex", alignItems: "center"}}>
-                    <img style={{height: 35, margin: 4}} src={twitter}/>
+                    <img style={{height: 35, margin: 4}} src={newTwitter}/>
                     <img style={{height: 35, margin: 4}} src={ig}/>
                     <img style={{height: 35, margin: 4}} src={youtube}/>
                     <img style={{height: 35, margin: 4}} src={twitch}/>
                     <img style={{height: 35, margin: 4}} src={kick}/>
                     <img style={{height: 35, margin: 4}} src={substack}/>
                 </div>
-                <p className="handle">@mlmstreamer</p>
+                <p className="handle">@quixoticodyssey</p>
             </div>
         </div>
     );
